b1976356d55059a9d4c0ca3849f23fdf323df3eb
gauravksaini23@gmail.com <gauravksaini23@gmail.com>
2022-05-01 01:13:04 +0530

    Practices

---
+++ Practice.StaticConstructor/Bus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.StaticConstructor
+{
+    public class Bus
+    {
+        public Bus(int times)
+        {
+            Console.WriteLine($"Bus default constructor for the {times} time");
+        }
+        static Bus()
+        {
+            Console.WriteLine("Bus static constructor");
+        }   
+    }
+}
---
+++ Practice.StaticConstructor/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Practice.StaticConstructor;
+
+var bus = new Bus(1);
+var bus1 = new Bus(2);
+
